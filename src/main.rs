5ac0477109a8ca47eedcdaf9c775b7a8f2431a7f
bouzuya <m@bouzuya.net>
2022-07-11 22:05:03 +0900

    fix scraping

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,80 @@
+use anyhow::Context;
 use regex::Regex;
 use reqwest::header::USER_AGENT;
-use reqwest::Response;
 use scraper::{Html, Selector};
-use std::cmp;
 use std::env;
 use url::Url;
 
 struct Item {
     title: String,
-    points: i32,
+    point: i32,
     price: i32,
 }
 
-async fn http_get(url: Url) -> anyhow::Result<Response> {
+async fn http_get(url: Url) -> anyhow::Result<String> {
     let product = "oikurs";
     let version = "0.1.0";
     let client = reqwest::Client::new();
-    Ok(client
+    let response = client
         .get(url)
         .header(USER_AGENT, format!("{}/{}", product, version))
         .send()
-        .await?)
+        .await?;
+    Ok(response.text().await?)
 }
 
-async fn parse(response: Response) -> anyhow::Result<Item> {
-    let out = response.text().await?;
-    let html = out.to_string();
+async fn parse(html: String) -> anyhow::Result<Item> {
     let document = Html::parse_document(&html);
     // select title element
-    let title_selector = Selector::parse(".series-detail-title").unwrap();
+    let title_selector = Selector::parse("#collection-title").expect("invalid selector");
     let title = document
         .select(&title_selector)
-        .nth(0)
-        .map_or(String::new(), |e| {
-            e.text().fold(String::new(), |a, s| a + s)
-        });
-    // select regular price element
-    let regular_price_selector = Selector::parse(".series-price-box-price.regular-price").unwrap();
-    let regular_price = document
-        .select(&regular_price_selector)
-        .nth(0)
-        .map(|e| e.text().fold(String::new(), |a, s| a + s))
-        .map_or(0, |s| {
-            Regex::new("[^0-9]")
-                .unwrap()
-                .replace_all(&s, "")
-                .parse::<i32>()
-                .unwrap()
-        });
+        .next()
+        .map(|e| e.text().fold(String::new(), |a, s| a + s.trim()))
+        .context("title not found")?;
     // select price element
-    let bulk_price_selector = Selector::parse("#series-bulkPrice-text").unwrap();
-    let bulk_price = document
-        .select(&bulk_price_selector)
-        .nth(0)
-        .map(|e| e.text().fold(String::new(), |a, s| a + s))
-        .map_or(0, |s| {
-            Regex::new("[^0-9]")
-                .unwrap()
-                .replace_all(&s, "")
-                .parse::<i32>()
-                .unwrap()
-        });
-    let price = cmp::max(regular_price, bulk_price);
-    // select discount price element
-    let discount_price_selector =
-        Selector::parse(".series-price-box-price.discount-price").unwrap();
-    let discount_price = document
-        .select(&discount_price_selector)
-        .nth(0)
-        .map(|e| e.text().fold(String::new(), |a, s| a + s))
-        .map_or(0, |s| {
-            Regex::new("[^0-9]")
-                .unwrap()
-                .replace_all(&s, "")
-                .parse::<i32>()
-                .unwrap()
-        });
-    // select points element
-    let amazon_points_selector = Selector::parse(".series-price-box-price.amazon-points").unwrap();
-    let amazon_points = document
-        .select(&amazon_points_selector)
-        .nth(0)
-        .map(|e| e.text().fold(String::new(), |a, s| a + s))
-        .map_or(0, |s| {
-            Regex::new("([0-9]+)ポイント")
-                .unwrap()
-                .captures(&s)
-                .map_or(0, |m| m.get(1).unwrap().as_str().parse::<i32>().unwrap())
-        });
-    let points = cmp::max(regular_price - discount_price, amazon_points);
+    let prices = (0..4)
+        .filter_map(|i| {
+            let price_selector =
+                Selector::parse(format!("#hulk_buy_price_popover_volume_{i}").as_str())
+                    .expect("invalid selector");
+            document
+                .select(&price_selector)
+                .next()
+                .map(|e| e.text().fold(String::new(), |a, s| a + s.trim()))
+                .and_then(|s| {
+                    Regex::new("[^0-9]")
+                        .expect("invalid regex")
+                        .replace_all(&s, "")
+                        .parse::<i32>()
+                        .ok()
+                })
+                .map(|price| (i, price))
+        })
+        .collect::<Vec<(usize, i32)>>();
+    let price = prices.last().context("price not found")?.1;
+    // select point element
+    let points = (0..4)
+        .filter_map(|i| {
+            let price_selector = Selector::parse(format!("#hulk_buy_points_volume_{i}").as_str())
+                .expect("invalid selector");
+            document
+                .select(&price_selector)
+                .next()
+                .map(|e| e.text().fold(String::new(), |a, s| a + s.trim()))
+                .and_then(|s| {
+                    Regex::new("([0-9]+)pt")
+                        .expect("invalid regex")
+                        .captures(&s)
+                        .and_then(|m| m.get(1).unwrap().as_str().parse::<i32>().ok())
+                })
+                .map(|price| (i, price))
+        })
+        .collect::<Vec<(usize, i32)>>();
+    let point = points.last().context("price not found")?.1;
     Ok(Item {
         title,
-        points,
+        point,
         price,
     })
 }
@@ -104,10 +87,10 @@ async fn main() -> anyhow::Result<()> {
     let item = parse(response).await?;
     println!(
         "{} = {} - {} ({}%): {}",
-        item.price - item.points,
+        item.price - item.point,
         item.price,
-        item.points,
-        (f64::from(item.points) / f64::from(item.price) * 100.0).round(),
+        item.point,
+        (f64::from(item.point) / f64::from(item.price) * 100.0).round(),
         item.title
     );
     Ok(())
